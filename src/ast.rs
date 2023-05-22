98e4e5ae36dd4de28f8dd070c392b38ef3501618
Joel Milligan <joelmilligan24@gmail.com>
2023-05-22 21:13:50 +0800

    Parse let statements with errors

--- src/ast.rs
+++ src/ast.rs
@@ -5,6 +5,7 @@ trait Node {
     fn token_literal(&self) -> String;
 }
 
+#[derive(Debug)]
 enum Statement {
     Let {
         token: Token,
@@ -25,10 +26,12 @@ impl Node for Statement {
     }
 }
 
+#[derive(Debug)]
 enum Expression {
     Temporary,
 }
 
+#[derive(Debug)]
 struct Identifer {
     token: Token,
     value: String,
@@ -58,17 +61,19 @@ struct Parser {
     lexer: Lexer,
     current_token: Token,
     peek_token: Token,
+    errors: Vec<String>,
 }
 
 impl Parser {
     fn new(mut lexer: Lexer) -> Self {
-        let peek_token = lexer.next_token();
         let current_token = lexer.next_token();
+        let peek_token = lexer.next_token();
 
         Parser {
             lexer,
             current_token,
             peek_token,
+            errors: vec![],
         }
     }
 
@@ -78,15 +83,32 @@ impl Parser {
     }
 
     fn parse_statement(&mut self) -> Option<Statement> {
+        println!("START STATEMENT: {:?}", self.current_token);
         match &self.current_token.kind {
-            Kind::Let => Some(Statement::Let {
-                token: self.current_token.clone(),
-                name: Identifer {
+            Kind::Let => {
+                if !self.expect_peek(Kind::Ident) {
+                    return None;
+                }
+
+                let name = Identifer {
                     token: self.current_token.clone(),
                     value: self.current_token.literal.clone(),
-                },
-                value: Expression::Temporary,
-            }),
+                };
+
+                if !self.expect_peek(Kind::Assign) {
+                    return None;
+                }
+
+                while self.current_token.kind != Kind::SemiColon {
+                    self.next_token();
+                }
+
+                Some(Statement::Let {
+                    token: self.current_token.clone(),
+                    name,
+                    value: Expression::Temporary,
+                })
+            }
             _ => None,
         }
     }
@@ -94,8 +116,11 @@ impl Parser {
     fn parse_program(&mut self) -> Program {
         let mut program = Program { statements: vec![] };
 
+        println!("START PROGRAM: {:?}", self.current_token);
+
         while self.current_token.kind != Kind::Eof {
             let statement = self.parse_statement();
+            println!("STATEMENT: {statement:?}");
             if let Some(s) = statement {
                 program.statements.push(s);
             }
@@ -103,6 +128,26 @@ impl Parser {
         }
 
         program
+    }
+
+    fn peek_error(&mut self, token_kind: Kind) {
+        self.errors.push(format!(
+            "expected next token to be {token_kind:?}, got {:?} instead",
+            self.peek_token.kind
+        ));
+    }
+
+    fn expect_peek(&mut self, expected_token: Kind) -> bool {
+        println!("PEEK: {:?}", self.peek_token);
+        if expected_token == self.peek_token.kind {
+            println!("TOKEN SUCCESS: {expected_token:?}");
+            self.next_token();
+            return true;
+        } else {
+            println!("TOKEN FAIL: {expected_token:?}");
+            self.peek_error(expected_token);
+            return false;
+        }
     }
 }
 
@@ -122,7 +167,18 @@ mod tests {
                 token: _,
                 name,
                 value: _,
-            } => name.value != let_name && name.token_literal() != let_name,
+            } => name.value == let_name && name.token_literal() == let_name,
+        }
+    }
+
+    fn check_parser_errors(parser: &Parser) {
+        if parser.errors.len() != 0 {
+            println!("Got {} errors.", parser.errors.len());
+            for error in &parser.errors {
+                println!("ERROR: {error}");
+            }
+
+            assert!(false);
         }
     }
 
@@ -130,8 +186,8 @@ mod tests {
     fn let_statement() {
         // Arrange
         let input = r#"
-            let x = 5;
-            let y = 10;
+            let x = 5; 
+            let y = 10; 
             let foobar = 838383;
         "#
         .to_string();
@@ -144,6 +200,7 @@ mod tests {
         let program = parser.parse_program();
 
         // Assert
+        check_parser_errors(&parser);
         assert_eq!(program.statements.len(), 3);
         for (i, t) in tests.iter().enumerate() {
             let statement = &program.statements[i];
